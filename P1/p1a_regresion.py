6a4c6e93069e20aa4b63eaa2c4d36f34a2866eb8
dsoldevila <david.sundevila@gmail.com>
2018-09-27 18:15:01 +0200

    some work

---
+++ P1/p1a_regresion.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 27 17:54:15 2018
+
+@author: David
+"""
+
+import os
+import glob
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import make_regression
+from matplotlib import pyplot as plt
+
+
+
+def mean_squared_error(target, reg):
+    """
+    Calcula l'error quadràtic mitjà comés pel regressor
+    @param target
+    @param reg 
+    """
+    return ((target - reg)**2).mean()
+
+def load_dataset(path):
+    """
+    Carrega la base de dades
+    @param path
+    """
+    #if path not full
+    path = os.path.join(os.getcwd(), path)
+    return
+    
+def split_data():
+    return
+    
+def standarize():
+    return
+    
