07a79cd8c576a17b2004674da582709a0657442c
dsoldevila <david.sundevila@gmail.com>
2018-09-30 17:29:25 +0200

    Working on regression

--- P1/p1a_regresion.py
+++ P1/p1a_regresion.py
@@ -5,6 +5,16 @@ Created on Thu Sep 27 17:54:15 2018
 @author: David
 """
 
+"""NOTES
+
+    1- He obviat marca i model, ja que np.genfromtxt no suporta strings
+    i no sembla que tingui sentit tenir-los en compte com a mètrica de
+    rendiment. Mirar si tenen correlació, un "no crec" no és vàlid
+
+
+"""
+
+
 import os
 import numpy as np
 from sklearn.linear_model import LinearRegression
@@ -24,12 +34,27 @@ def mean_squared_error(target, reg):
 def load_dataset(path):
     """
     Carrega la base de dades
-    @param path
+    @param path (abs or not)
     """
     if(os.path.isabs(path) == False):
         path = os.path.join(os.getcwd(), path)
+    data = np.genfromtxt(path, delimiter=",")
     
-    return
+    attributes = data[:, 2:8]
+    goal = data[:, 8]
+    db_guess = data[:, 9]
+    
+
+    goal = goal.reshape(goal.shape[0], 1)
+    db_guess = db_guess.reshape(db_guess.shape[0], 1)
+    
+    return attributes, goal, db_guess
+
+def regression(x, y):
+    regr = LinearRegression()
+    regr.fit(x, y)
+    return regr
+    
     
 def split_data():
     return
@@ -37,3 +62,21 @@ def split_data():
 def standarize():
     return
     
+
+attributes, goal, dg= load_dataset("Database\machine.data.txt")
+at = attributes[:, 2].reshape(attributes.shape[0], 1)
+
+print(at.shape)
+print(goal.shape)
+
+regr = regression(at, goal)
+predicted = regr.predict(at)
+
+plt.plot(at, predicted, 'r')
+plt.scatter(at, goal)
+
+print("MSE: ")
+print(mean_squared_error(goal, predicted))
+
+print("SCORE: ")
+print(regr.score(at, goal))
