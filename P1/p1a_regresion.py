6faa3fe1e85f3d92a44a0644f5e5e0dad8513419
dsoldevila <david.sundevila@gmail.com>
2018-10-10 11:18:34 +0200

    Código regressión apartado B

--- P1/p1a_regresion.py
+++ P1/p1a_regresion.py
@@ -9,21 +9,22 @@ Created on Thu Sep 27 17:54:15 2018
 """
 
 
-import os
+#import os
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.datasets import make_regression
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import axes3d, Axes3D
 
 class Regression:
 
-    def __init__(self, X_min, X_max, target, db_col, path):
+    def __init__(self, X_min, X_max, target, db_col, path, train_ratio=0.8):
         self.X_MIN = X_min #Attributes' range of columns in DB
         self.X_MAX = X_max
         self.TARGET= target #Column index of target
         self.DB_COL = db_col
         self.loadDataset(path)
-        self.splitDataset()
+        self.splitDataset(train_ratio)
         return
         
         
@@ -34,6 +35,43 @@ class Regression:
         @param reg 
         """
         return ((y-prediction)**2).mean()
+    
+    def computeMSE(self, X_train, X_val, indices, plot):
+        mse_list = []
+    
+        xt = X_train[:, 0].reshape(X_train.shape[0], 1)
+        xv = X_val[:, 0].reshape(X_val.shape[0], 1)
+        
+        linear_regression = self.regression(xt, self.y_train)
+        predicted = linear_regression.predict(xv)
+    
+        mse = self.meanSquaredError(self.y_val, predicted)
+        mse_list.append((self.DB_COL[indices[0]], mse))
+        
+        if plot:
+            self.plotR(xv, self.DB_COL[indices[0]], self.y_val, predicted)
+        
+        lowest_mse = mse
+        lowest_mse_i = indices[0]
+       
+        for i in range(1, len(indices)):
+            xt = X_train[:, i].reshape(X_train.shape[0], 1)
+            xv = X_val[:, i].reshape(X_val.shape[0], 1)
+        
+            linear_regression = self.regression(xt, self.y_train)
+            predicted = linear_regression.predict(xv)
+    
+            mse = self.meanSquaredError(self.y_val, predicted)
+            mse_list.append((self.DB_COL[indices[i]], mse))
+            
+            if plot:
+                self.plotR(xv, self.DB_COL[indices[i]], self.y_val, predicted)
+            
+            con = (lowest_mse<mse)
+            lowest_mse = lowest_mse if con else mse
+            lowest_mse_i = lowest_mse_i if con else indices[i]
+            
+        return mse_list, self.DB_COL[lowest_mse_i], lowest_mse
     
     def loadDataset(self, path):
         """
@@ -48,7 +86,7 @@ class Regression:
         self.y = self.y.reshape(self.y.shape[0], 1)     
         return
         
-    def splitDataset(self, train_ratio=0.8):
+    def splitDataset(self, train_ratio):
         """
         Divideix aleatòriament la base de dades en training set i validation set
         """
@@ -93,55 +131,91 @@ class Regression:
         plt.scatter(x, y)
         plt.plot(x, predicted, color="r")
         return
+    def plotR3D(self, X_v, predicted, x1_name, x2_name):
+        # Afegim els 1's
+        A = np.hstack((X_v,np.ones([X_v.shape[0],1])))
+        w = np.linalg.lstsq(A,predicted, rcond=-1)[0]
+        
+        #Dibuixem
+        #1r creem una malla acoplada a la zona de punts per tal de representar el pla
+        malla = (range(20) + 0 * np.ones(20)) / 10 
+        malla_x1 =  malla * (max(X_v[:,0]) - min(X_v[:,0]))/2 + min(X_v[:,0])
+        malla_x2 =  malla * (max(X_v[:,1]) - min(X_v[:,1]))/2 + min(X_v[:,1])
+        
+        #la fucnio meshgrid ens aparella un de malla_x1 amb un de malla_x2, per atot
+        #element de mallax_1 i per a tot element de malla_x2.
+        xplot, yplot = np.meshgrid(malla_x1 ,malla_x2)
+        
+        #ara creem la superficies que es un pla
+        zplot = w[0] * xplot + w[1] * yplot + w[2]
+        
+        #Dibuixem punts i superficie
+        plt3d = plt.figure('Coeficiente prismatico -- Relacio longitud desplacament 3D').gca(projection='3d')
+        plt.xlabel(x1_name)
+        plt.ylabel(x2_name)
+        plt3d.plot_surface(xplot,yplot,zplot, color='red')
+        plt3d.scatter(X_v[:,0],X_v[:,1],self.y_val)
+        
+        return
 
-
+def p1a_b(path):
+    db_col = ["vendor", "Model", "MYCT", "MMIN", "MMAX", "CACH", "CHMIN", "CHMAX", "PRP", "ERP"]
+    x_min = 2
+    x_max = 8
+    target = 8
     
-def p1a_c():
+    print("#Trobar el millor train ratio")
+    train_ratios = (0.8, 0.7, 0.6, 0.5)
+    mse_list = []
+    regr = Regression(x_min, x_max, target, db_col, path, train_ratio=train_ratios[0])
+    X_t = regr.X_train[:, 0:3]
+    X_v = regr.X_val[:, 0:3]
+    linear_regression = regr.regression(X_t, regr.y_train)
+    predicted = linear_regression.predict(X_v)
+    mse = regr.meanSquaredError(regr.y_val, predicted)
+    mse_list.append((train_ratios[0], mse))
+    best_tr = train_ratios[0]
+    lowest_mse = mse
+    
+    for tr in train_ratios[1:]:
+        regr.splitDataset(train_ratio=tr)
+        X_t = regr.X_train[:, 0:3]
+        X_v = regr.X_val[:, 0:3]
+        linear_regression = regr.regression(X_t, regr.y_train)
+        predicted = linear_regression.predict(X_v)
+        mse = regr.meanSquaredError(regr.y_val, predicted)
+        mse_list.append((tr, mse))
+        con = (lowest_mse < mse)
+        best_tr = best_tr if con else tr
+        lowest_mse = lowest_mse if con else mse
+    print(mse_list)
+    print(str(best_tr)+" is the best train ratio\n")
+    
+    print("#Representació 3D de la regressió dels attributs MMIN, MMAX")
+    regr.splitDataset(train_ratio=best_tr)
+    X_t = regr.X_train[:, 0:3]
+    X_v = regr.X_val[:, 0:3]
+    linear_regression = regr.regression(X_t, regr.y_train)
+    predicted = linear_regression.predict(X_v)
+    regr.plotR3D(X_v[:, 1:], predicted, "MMIN", "MMAX")
+    return
+    
+def p1a_c(path):
     """
     Calcula MSEs i imprimeix les rectes de regressió per aca attribut
     """
     db_col = ["vendor", "Model", "MYCT", "MMIN", "MMAX", "CACH", "CHMIN", "CHMAX", "PRP", "ERP"]
-    regr = Regression(2, 8, 8, db_col, "Database/machine.data.txt")
-    
+    x_min = 2
+    x_max = 8
+    target = 8
+    regr = Regression(x_min, x_max, target, db_col, path)
     
     #Calculo MSEs e imprimir regresión
-    mse_list = []
-    
     print("#Compute MSEs with raw attributes and print linear regression:")
-    xt = regr.X_train[:, 0].reshape(regr.X_train.shape[0], 1)
-    xv = regr.X_val[:, 0].reshape(regr.X_val.shape[0], 1)
-    
-    linear_regression = regr.regression(xt, regr.y_train)
-    predicted = linear_regression.predict(xv)
-
-    mse = regr.meanSquaredError(regr.y_val, predicted)
-    mse_list.append((db_col[0+regr.X_MIN], mse))
-    
-    regr.plotR(xv, db_col[0], regr.y_val, predicted)
-    
-    lowest_mse = mse
-    lowest_mse_i = 0
-   
-    for i in range(1, regr.X_MAX-regr.X_MIN):
-        xt = regr.X_train[:, i].reshape(regr.X_train.shape[0], 1)
-        xv = regr.X_val[:, i].reshape(regr.X_val.shape[0], 1)
-    
-        linear_regression = regr.regression(xt, regr.y_train)
-        predicted = linear_regression.predict(xv)
-
-        mse = regr.meanSquaredError(regr.y_val, predicted)
-        mse_list.append((db_col[0+regr.X_MIN], mse))
-    
-        regr.plotR(xv, db_col[0+regr.X_MIN], regr.y_val, predicted)
-        
-        con = (lowest_mse<mse)
-        lowest_mse = lowest_mse if con else mse
-        lowest_mse_i = lowest_mse_i if con else i
-        
+    mse_list, lm_name, lowest_mse = regr.computeMSE(regr.X_train, regr.X_val, np.arange(x_min, x_max), True)
     print("MSEs:")
     print(str(mse_list)+"\n")
-    print(db_col[lowest_mse_i+regr.X_MIN]+ " has the lowest mse: "+str(lowest_mse)+"\n")
-    
+    print(lm_name+ " has the lowest mse: "+str(lowest_mse)+"\n")
     
     #Plot standard attributes
     print("#Plot standarized variables\n")
@@ -149,6 +223,7 @@ def p1a_c():
     for i in range(6):
         x = regr.X_train_std[:, i].reshape(regr.X_train_std.shape[0], 1)
         regr.plotX(x, db_col[i+regr.X_MIN])
+        
     #TODO Calcular MSEs con las variables normalizadas
     
     #Recompute regression with top3 attributes
@@ -159,8 +234,10 @@ def p1a_c():
     predicted = linear_regression.predict(X_v)
 
     mse = regr.meanSquaredError(regr.y_val, predicted)
-    print("#Best attreibutes mse: "+str(mse))
-          
+    print("#Best attreibutes (MYCT, MMIN, MMAX) mse: "+str(mse))
+     
     return
 
-p1a_c()
+if __name__ == "__main__":
+    #p1a_c("Database/machine.data.txt")
+    p1a_b("Database/machine.data.txt")
