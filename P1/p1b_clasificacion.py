46f418ce5cae36fa9fe421f5385ccaf936b59927
dgsanchez <david.sanchezgon@e-campus.uab.cat>
2018-10-18 22:19:08 +0200

    Reorganization of p1b

--- P1/p1b_clasificacion.py
+++ P1/p1b_clasificacion.py
@@ -6,47 +6,47 @@ import numpy as np
 from sklearn.model_selection import LeaveOneOut, KFold
 from sklearn import svm
 import matplotlib.pyplot as plt
+import pandas as pd
 
-from p1a_regresion import load_dataset, split_dataset
+class Clasificacion:
 
+	def __init__(self, path, ATT_MIN, ATT_MAX, TARGET, DB_GUESS, DB_COL):
+		self.att_min = ATT_MIN
+		self.att_max = ATT_MAX
+		self.target = TARGET
+		self.db_guess = DB_GUESS
+		self.db_col = DB_COL
+		self.load_Dataset(path)
 
-"""
-def split_data(x, y, train_ratio=0.8):
-	indices = np.arange(x.shape[0])
-	np.random.shuffle(indices)
-	n_train = int(np.floor(x.shape[0]*train_ratio))
+	def load_Dataset(self, path):
+		data = np.genfromtxt(path, delimiter=",")
 
-	indices_train = indices[:n_train]
-	indices_val = indices[-n_train:]
+		self.X = data[:, self.att_min:self.att_min]
+		self.Y = data[:, self.target]
 
-	x_train = x[indices_train, :]
-	y_train = y[indices_train]
-
-	x_val = x[indices_val, :]
-	y_val = y[indices_val]
-	
-	return x_train, y_train, x_val, y_val
-"""
-
-
-def train_svm(x, y, kernel='linear', C=0.01, gamma=0.001, probability=True):
-	if(kernel =='linear'):
-		svclin = svm.SVC(C=C, kernel=kernel, gamma=gamma, probability=probability)
-
-	if(kernel =='poly'):
-		svclin = svm.SVC(C=C, kernel=kernel, gamma=gamma, probability=probability)
-
-	if(kernel =='rbf'):
-		svclin = svm.SVC(C=C, kernel=kernel, gamma=gamma, probability=probability)
-
-	# l'entrenem
-	return svclin.fit(x, y)
+		self.Y = self.Y.reshape(self.Y.shape[0], 1)     
+		return
 
 
-def p1b_c():
+	def train_svm(self, kernel='linear', C=0.01, gamma=0.001, probability=True):
+		if(kernel =='linear'):
+			svclin = svm.SVC(C=C, kernel=kernel, gamma=gamma, probability=probability)
+
+		if(kernel =='poly'):
+			svclin = svm.SVC(C=C, kernel=kernel, gamma=gamma, probability=probability)
+
+		if(kernel =='rbf'):
+			svclin = svm.SVC(C=C, kernel=kernel, gamma=gamma, probability=probability)
+
+		# l'entrenem
+		return svclin.fit(self.X, self.Y)
+
+
+def p1b_c(path):
 	"""
 	Funció principal per a l'apartat C de la pràctica 1b.
 	"""
+
 	ATT_MIN = 2 #Attributes' range of columns in DB
 	ATT_MAX = 9
 	TARGET = 9 #Column index of target
@@ -54,19 +54,9 @@ def p1b_c():
 
 	DB_COL = np.array(["vendor", "Model", "MYCT", "MMIN", "MMAX", "CACH", "CHMIN", "CHMAX", "PRP", "ERP"])
 
-	at_train, target_train, at_val, target_val = load_dataset(os.path.join("Database","machine.data.txt"), ATT_MIN, ATT_MAX, TARGET, DB_COL)
+	clas = Clasificacion(path, ATT_MIN, ATT_MAX, TARGET, DB_GUESS, DB_COL)
 
-	# We use the Heatmap to know which variables are the most correlated
-	# from the estimated performance.
-	indexes = np.arange(0, ATT_MAX - ATT_MIN, dtype=int)
-	df2 = pd.DataFrame(at_train[:, range(0, 7)], columns = DB_COL[indexes])
-	df2['ERP'] = target_train
-	corrmat = df2.corr()
-
-
-
-def main():
-	p1b_c()
 
 if __name__ == "__main__":
-	main()
+    path = os.path.join("Database", "machine.data.txt")
+    p1b_c(path)
